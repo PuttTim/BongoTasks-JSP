02aa553c1def0794ea4453586bab849afdc530f1
PuttTim <yannaputt@gmail.com>
2023-02-05 05:33:18 +0800

    ⚡ Removed redundant methods from Task.java

--- src/main/java/com/example/bongotasks/Task.java
+++ src/main/java/com/example/bongotasks/Task.java
@@ -18,33 +18,17 @@ public class Task {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
     public boolean isStatus() {
         return status;
     }
-
-    public void setStatus(boolean status) {
-        this.status = status;
-    }
-
+    
 
 }
